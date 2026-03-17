ac20e8144d41bd337df3a73d244380d15370a2d6
shubhiscoding <mygitcoin@gmail.com>
2026-03-18 02:46:18 +0530

    use Arc and with state for global state

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use axum::{Json, Router, extract::Path, http::StatusCode, routing::get};
+use std::{collections::HashMap, sync::Arc};
+
+use axum::{Json, Router, extract::{Path, State}, http::StatusCode, routing::get};
 use serde::Serialize;
 #[derive(Serialize)]
 struct Token {
@@ -11,11 +13,23 @@ struct  ErrorResponse {
     error: String,
 }
 
+struct AppState {
+    tokens: HashMap<String, u64>,
+}
+
 #[tokio::main]
 async fn main() {
+
+    let mut tokens = HashMap::new();
+    tokens.insert("MorphToken".to_string(), 1_000_000);
+    tokens.insert("RustToken".to_string(), 500_000);
+
+    let state = Arc::new(AppState {tokens});
+    
     let app = Router::new()
     .route("/", get(root))
-    .route("/token/{name}", get(get_token));
+    .route("/token/{name}", get(get_token))
+    .with_state(state);
 
     let listeners = tokio::net::TcpListener::bind("127.0.0.1:3000")
     .await
@@ -31,18 +45,18 @@ async fn root() -> &'static str {
     "Hello from Rust server!!"
 }
 
-async fn get_token(Path(name): Path<String>) -> Result<Json<Token>, (StatusCode, Json<ErrorResponse>)> {
-    let supply = match name.as_str() {
-        "MorphToken" => 1000000,
-        "RustToken" => 500000,
-        _ => {
+async fn get_token( State(state): State<Arc<AppState>>, Path(name): Path<String>) -> Result<Json<Token>, (StatusCode, Json<ErrorResponse>)> {
+    match state.tokens.get(&name) {
+        Some(supply) => {
+            let token = Token {
+                name,
+                supply: *supply,
+            };
+            Ok(Json(token))
+        }
+        None => {
             let err_msg = ErrorResponse{error: String::from("Token Not Found")};
             return Err((StatusCode::NOT_FOUND, Json(err_msg)));
         }
-    };
-    let token = Token {
-        name,
-        supply
-    };
-    Ok(Json(token))
+    }
 }
