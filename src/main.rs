1da26e0e3e8cbfc0f202280990f9c3db53e8b343
shubhiscoding <mygitcoin@gmail.com>
2026-03-18 01:43:08 +0530

    create endpoint with input in path

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use axum::{Router, routing::get};
+use axum::{Json, Router, extract::Path, routing::get};
+use serde::Serialize;
+#[derive(Serialize)]
+struct Token {
+    name: String,
+    supply: u64,
+}
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-    .route("/", get(root));
+    .route("/", get(root))
+    .route("/token/{name}", get(get_token));
 
     let listeners = tokio::net::TcpListener::bind("127.0.0.1:3000")
     .await
@@ -17,4 +24,17 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello from Rust server!!"
+}
+
+async fn get_token(Path(name): Path<String>) -> Json<Token> {
+    let supply = match name.as_str() {
+        "MorphToken" => 1000000,
+        "RustToken" => 500000,
+        _ => 0,
+    };
+    let token = Token {
+        name,
+        supply
+    };
+    Json(token)
 }
